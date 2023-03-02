a229267194ff0409e37b3da6602126b96dfeb6e9
marximillion <marximillion@gmail.com>
2023-03-02 16:09:18 -0700

    Form passes correct values to javascript

--- app.py
+++ app.py
@@ -40,3 +40,28 @@ Session(app)
 @app.route("/", methods=['GET'])
 def main():
     return render_template("index.html")
+
+@app.route("/api", methods=["GET", "POST"])
+def api():
+
+    #fullcode = code.zfill(10)
+    # print(fullcode)
+    res = requests.get("https://data.calgary.ca/resource/c2es-76ed.geojson")
+
+    #data = res.json()
+    #data_str = json.dumps(data, indent=4)
+
+    return render_template("api.html", data=res.text)
+
+@app.route('/search', methods=["GET", "POST"])
+def search():
+    start_date = request.form.get('start_date')
+    end_date = request.form.get('end_date')
+    #url = f'https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate > '{start_date}' and issueddate < '{end_date}'&$select=issueddate,workclassgroup,contractorname,communityname,originaladdress,latitude,longitude'
+    url = f'https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate > "{start_date}" and issueddate < "{end_date}"&$select=issueddate,workclassgroup,contractorname,communityname,originaladdress,latitude,longitude'
+    response = requests.get(url)
+    data = response.json()
+    return render_template("search.html", data=data)
+
+if __name__ == 'main':
+    app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,13 +1,14 @@
 console.log("Success loading .js script");
 $(document).ready(function() {
-    var myMap = L.map('map').setView([51.505, -0.09], 13);
-
+    var myMap = L.map('map').setView([51.0468, -114.0500], 10);
+    
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(myMap);
+    
+}).addTo(myMap);
 
-    var marker = L.marker([51.5, -0.09]).addTo(myMap);
+    var marker = L.marker([51.0468, -114.0500]).addTo(myMap);
 });
   
 
