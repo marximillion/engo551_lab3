17ea1ebbe33c1819c39a35a41b800212c1d10811
marximillion <marximillion@gmail.com>
2023-03-13 00:26:59 -0600

    Mapbox uploaded successfully

--- static/script.js
+++ static/script.js
@@ -111,11 +111,14 @@ $(document).ready(function () {
     iconColor: "blue",
   });
 
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-    maxZoom: 18,
-  }).addTo(map);
+  var baseLayer = L.tileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+    }
+  ).addTo(map);
 
   L.marker([51.0447, -114.0719], { icon: cityHall })
     .addTo(map)
@@ -126,28 +129,70 @@ $(document).ready(function () {
   //.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
 
   const form = document.getElementById("dates");
+  /*
+  var mapbox1 = L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: "zifanzhang12345/clezxrw8c000401qm3j7rnuda", // Replace with your own Mapbox style ID
+      accessToken:
+        "pk.eyJ1IjoiemlmYW56aGFuZzEyMzQ1IiwiYSI6ImNsZXo0aWt6bDBvajIzeG8wdHFjYmZydzgifQ.14P31UjREuVFl0CfgVeBLg",
+    }
+  ).addTo(map);
+  */
+  var mapbox2 = L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: "ttoews45/cleuc9bej000f01p5r173pdy8", // Replace with your own Mapbox style ID
+      accessToken:
+        "pk.eyJ1IjoidHRvZXdzNDUiLCJhIjoiY2xldWJ5MjQwMDZkNzNxcnRuZHd5eXpqOSJ9.vxwK3vw-CjJH5GF9ihrpsg&zoomwheel=true&fresh=true#9.88/51.0167/-114.0239",
+    }
+  ).addTo(map);
+
+  var Layers = {
+    "Building Permit": baseLayer,
+    Tyson: mapbox2,
+  };
 
   form.addEventListener("submit", (event) => {
     map.eachLayer(function (layer) {
       map.removeLayer(layer);
     });
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      maxZoom: 18,
-    }).addTo(map);
+    BuildingPermits = L.tileLayer(
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+      }
+    ).addTo(map);
 
     const startdate = window.S_Date.slice(0, -1);
     const enddate = window.E_Date.slice(0, -1);
 
     var markers = L.markerClusterGroup();
-    console.log(`https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate > \'${startdate}\' and issueddate< \'${enddate}\'`);
+
+    console.log(mapbox2._url);
     fetch(
       `https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate > \'${startdate}\' and issueddate< \'${enddate}\'`
     )
       .then((response) => response.json())
       .then((data) => {
+        var mapbox2 = L.tileLayer(
+          "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+          {
+            attribution:
+              'Map data &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: "ttoews45/cleuc9bej000f01p5r173pdy8", // Replace with your own Mapbox style ID
+            accessToken:
+              "pk.eyJ1IjoidHRvZXdzNDUiLCJhIjoiY2xldWJ5MjQwMDZkNzNxcnRuZHd5eXpqOSJ9.vxwK3vw-CjJH5GF9ihrpsg&zoomwheel=true&fresh=true#9.88/51.0167/-114.0239",
+          }
+        ).addTo(map);
+
         // Use the GeoJSON data here
         var temp = L.geoJSON(data, {
           style: myStyle,
@@ -168,3 +213,6 @@ $(document).ready(function () {
     console.log("I get:", enddate);
   });
 });
+
+// <iframe width='100%' height='400px' src="https://api.mapbox.com/styles/v1/ttoews45/cleuc9bej000f01p5r173pdy8.html?title=false&access_token=pk.eyJ1IjoidHRvZXdzNDUiLCJhIjoiY2xldWJ5MjQwMDZkNzNxcnRuZHd5eXpqOSJ9.vxwK3vw-CjJH5GF9ihrpsg&zoomwheel=false#9.88/51.0167/-114.0239" title="Streets" style="border:none;"></iframe>
+// https://api.mapbox.com/styles/v1/ttoews45/cleuc9bej000f01p5r173pdy8.html?title=view&access_token=pk.eyJ1IjoidHRvZXdzNDUiLCJhIjoiY2xldWJ5MjQwMDZkNzNxcnRuZHd5eXpqOSJ9.vxwK3vw-CjJH5GF9ihrpsg&zoomwheel=true&fresh=true#9.88/51.0167/-114.0239
